bca50608a2ec224f5c1466164e7dd0273eb3d00a
Tegridy <jarjak4@gmail.com>
2022-07-28 10:34:30 +0200

    Project init

---
+++ src/main/java/com/hackathonorganizer/hackathonreadservice/HackathonReadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hackathonorganizer.hackathonreadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonReadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonReadServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathonorganizer/hackathonreadservice/HackathonReadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathonorganizer.hackathonreadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonReadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
